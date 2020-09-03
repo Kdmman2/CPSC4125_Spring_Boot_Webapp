e6830d9dbf994ec7d3d5fe5dd79ed8afff316b83
Kdmman2 <kdmman2@gmail.com>
2020-09-03 23:24:45 +0000

    Home, Contact, and About pages added

--- complete/src/main/java/com/example/springboot/HelloController.java
+++ complete/src/main/java/com/example/springboot/HelloController.java
@@ -17,6 +17,15 @@ public class HelloController {
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "greeting";
-	}
+    }
+    @GetMapping("/contact")
+	public String contact(Model model){
+        return "contact";
+    }
+    @GetMapping("/about")
+	public String about(Model model){
+        return "about";
+    }
+	
 
 }
